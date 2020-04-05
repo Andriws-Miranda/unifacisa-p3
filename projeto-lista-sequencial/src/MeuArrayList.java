7526855231da9aef3800a91fa78a8ccb1cc2900d
unknown <andriws2001@gmail.com>
2020-04-05 06:58:39 -0300

    Novas Atividades 05/04/2020

--- projeto-lista-sequencial/src/MeuArrayList.java
+++ projeto-lista-sequencial/src/MeuArrayList.java
@@ -9,24 +9,40 @@ public class MeuArrayList {
 	
 	public void add(Object obj) {
 
-		arrayInterno[inseridos] = obj;
-		inseridos++;
 		if (arrayInterno.length<inseridos) {
-			Object[] novoArray=new Object[ALLOCATION*2];
+			Object[] novoArray=new Object[arrayInterno.length+ALLOCATION];
 			for(int i=0; i<arrayInterno.length;i++) {
 				novoArray[i]=arrayInterno[i];
 			}
 			arrayInterno=novoArray;
 		}
+		arrayInterno[inseridos] = obj;
+		inseridos++; 
 	}
 
 	public void remove(Object obj) {
-
+		Object[] novoArray = new Object[arrayInterno.length-1];
+		for(int i=0;i<arrayInterno.length;i++) {
+			if(arrayInterno[i]!=obj) {
+				novoArray[i]=arrayInterno[i];
+			}else {
+				continue;
+			}
+		}
+		arrayInterno=novoArray;
 		inseridos--;
 	}
 
 	public void remove(int index) {
-
+		Object[] novoArray = new Object[arrayInterno.length-1];
+		for(int i=0; i<arrayInterno.length;i++) {
+			if(i!=index) {
+				novoArray[i]=arrayInterno[i];
+			}else {
+				continue;
+			}
+		}
+		arrayInterno=novoArray;
 		inseridos--;
 	}
 
